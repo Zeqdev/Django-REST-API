4f689bebdf6b43a92f049fda118b28e44f5a535b
Zeq <Ezequiellopez.dev@gmail.com>
2022-09-09 23:45:22 -0300

    Get method

--- api/views.py
+++ api/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views import View
+from .models import Company
 
 # Create your views here.
+
+class CompanyView(View):
+    def get(self, request):
+        companies = list(Company.objects.values())
+
+        if len(companies) > 0:
+            data = {'message': 'Success', 'companies': companies}
+        else:
+            data = {'message': 'Companies not found ...'}
+        return JsonResponse(data)
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
--- django_api_rest/urls.py
+++ django_api_rest/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import CompanyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/companies/', CompanyView.as_view()),
 ]
