639086e8890b4b1d9744078a962b7a613a0bb33c
Zeq <Ezequiellopez.dev@gmail.com>
2022-09-10 00:34:01 -0300

    Post method

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
 from django.views import View
 from .models import Company
+import json
 
 # Create your views here.
 
@@ -14,8 +17,22 @@ class CompanyView(View):
             data = {'message': 'Companies not found ...'}
         return JsonResponse(data)
 
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
     def post(self, request):
-        pass
+        json_data = json.loads(request.body)
+
+        Company.objects.create(
+            name=json_data['name'],
+            address=json_data['address'],
+            city=json_data['city'],
+            country=json_data['country'],
+        )
+
+        data = {'message': 'Company created successfully'}
+        return JsonResponse(data)
 
     def put(self, request):
         pass
